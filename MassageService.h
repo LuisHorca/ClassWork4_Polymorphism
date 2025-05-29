cf48d4723a1c97da2fb3922d6458bf6293fb5212
LuisHorca <a01713551@tec.mx>
2025-05-29 17:45:16 -0600

    Update MassageService.h

--- MassageService.h
+++ MassageService.h
@@ -1,28 +1,21 @@
 #ifndef MASSAGESERVICE_H
 #define MASSAGESERVICE_H
 
-#include "Service.h"
-#include <string>
-
 class MassageService : public Service {
-private:
-    std::string appointmentTime;
 public:
+    using Service::calculateFinalPrice;   
+
     MassageService(const std::string& name,
                    int duration,
                    double basePrice);
-
-    // overload to also set appointment time
     void registerService(const std::string& name,
                          int duration,
                          double basePrice,
                          const std::string& time);
-
-    // surcharge over 60 minutes
     double calculateFinalPrice() override;
-
-    // full booking summary
     void displaySummary(bool returningClient) override;
+private:
+    std::string appointmentTime;
 };
 
-#endif
+#endif 
