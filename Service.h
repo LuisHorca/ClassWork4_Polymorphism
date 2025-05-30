8799de380d2045b9b903b9250857d3b9536c4b73
LuisHorca <a01713551@tec.mx>
2025-05-29 18:19:45 -0600

    Update Service.h

--- Service.h
+++ Service.h
@@ -11,21 +11,18 @@ protected:
 public:
     Service(const std::string& name, int duration, double basePrice);
 
-    // (re)register core data
+ 
     virtual void registerService(const std::string& name,
                                  int duration,
                                  double basePrice);
 
-    // print name, duration, base price
     virtual void describeService();
 
-    // compute final price before any client‐type discount
     virtual double calculateFinalPrice() = 0;
 
-    // overloaded: apply returning‐client discount
     double calculateFinalPrice(bool returningClient);
 
-    // print full summary including discounts
+  
     virtual void displaySummary(bool returningClient) = 0;
 
     virtual ~Service();
